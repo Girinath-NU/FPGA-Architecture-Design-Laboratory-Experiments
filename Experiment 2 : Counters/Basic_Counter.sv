76a50b30ac6a78a904b0db178c24f52819eb19a1
Girinath NU <159938526+Girinath-NU@users.noreply.github.com>
2026-01-30 09:43:41 +0530

    Create Basic_Counter.sv

---
+++ Experiment 2 : Counters/Basic_Counter.sv
@@ -0,0 +1,14 @@
+module counter (
+    input  logic        clk,
+    input  logic        rst_n, // active-low reset
+    output logic [3:0]  count
+);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            count <= 4'b0000;
+        else
+            count <= count + 1'b1;
+    end
+
+endmodule
